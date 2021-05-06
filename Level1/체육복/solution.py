f7d387be070e76c06e65b5d09898f9412d022d73
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-05-07 01:51:04 +0900

    Level1. ~ 체육복 2.Wrong Answer

--- Level1/체육복/solution.py
+++ Level1/체육복/solution.py
@@ -2,22 +2,25 @@ def solution(n, lost, reserve):
     answer = n-len(lost)
 
     for i in range(len(reserve)):
-        reserve[i] = reserve[i]-1
+        if(reserve[i]-1>0):
+            reserve[i] = reserve[i] - 1
 
     for l in lost:
         if l in reserve:
             answer+=1
+            reserve.remove(l)
 
-    for i in range(len(reserve)):
-        reserve[i] = reserve[i] + 1
+    if answer != n-len(lost):
+        for i in range(len(reserve)):
+            reserve[i] = reserve[i] + 2
 
-    for l in lost:
-        if l in reserve:
-            answer += 1
+        for l in lost:
+            if l in reserve:
+                answer += 1
 
     return answer
 
-n = 5
-lost = [2, 4]
-reserve = [3]
+n = 3
+lost = [3]
+reserve = [1]
 print(solution(n, lost, reserve))
