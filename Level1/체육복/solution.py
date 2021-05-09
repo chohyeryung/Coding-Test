fac96278e9f0e0d2e72030f438bf0be75e510bbc
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-05-10 01:44:27 +0900

    Level1. ~ 체육복

--- Level1/체육복/solution.py
+++ Level1/체육복/solution.py
@@ -1,15 +1,16 @@
 def solution(n, lost, reserve):
-    answer = n-len(lost)
-    for l in lost:
-        if l-1 in reserve:
-            answer+=1
-            reserve.remove(l-1)
-        elif l+1 in reserve:
-            answer+=1
-            reserve.remove(l-1)
-    return answer
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    for l in _lost:
+        if l-1 in _reserve:
+            _reserve.remove(l-1)
+        elif l+1 in _reserve:
+            _reserve.remove(l+1)
+        else:
+            n-=1
+    return n
 
-n = 3
-lost = [3]
-reserve = [1]
+n = 5
+lost = [2, 4]
+reserve = [1, 3, 5]
 print(solution(n, lost, reserve))
